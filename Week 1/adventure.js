f3b79cad4fa553dc39b2bcd90969813a4fd97ed3
Marijn <marijndbesten@gmail.com>
2019-12-09 09:46:13 +0100

    Adventure game

---
+++ Week 1/adventure.js
@@ -0,0 +1,171 @@
+var title = document.getElementById("title");
+var description = document.getElementById("description");
+var button1 = document.getElementById("button1");
+var button2 = document.getElementById("button2");
+var button3 = document.getElementById("button3");
+var body = document.getElementsByTagName("BODY")[0];
+var item = document.getElementById("inventoryItem");
+  item.setAttribute("src", "https://vignette.wikia.nocookie.net/seaofthieves/images/1/1d/Coffre_du_marin.png/revision/latest?cb=20180617113421&path-prefix=fr");
+  item.setAttribute("width", "20");
+  item.setAttribute("height", "16");
+var itemclick = 0;
+
+start();
+
+function start(){
+	body.style.backgroundImage = "url('https://i.pinimg.com/originals/2f/88/5b/2f885b40be32fb4025bc0c6901dc3909.jpg')"
+	title.innerHTML = "Sea Adventure";
+	description.innerHTML = "Adventure gebaseerd op de game: Sea Of Thieves";
+	button1.innerHTML = "start";
+	button1.onclick = function(){
+		keuze1();
+	}
+	button1.style.marginLeft = "600px";
+	button2.style.display = "none";
+	button3.style.display = "none";
+	button1.style.marginTop = "425px";
+	description.style.display = "block";
+	button1.style.display = "inline";
+	item.style.display = "none";
+	title.style.backgroundColor = "#000";
+}
+
+function keuze1(){
+	body.style.backgroundImage = "url('https://i.pinimg.com/originals/2f/88/5b/2f885b40be32fb4025bc0c6901dc3909.jpg')"
+	title.innerHTML = "Sea Adventure";
+	button1.innerHTML = "Cannon Cove";
+	button3.innerHTML = "Shipwreck Bay";
+	description.style.display = "none";
+	button2.innerHTML = "Back";
+	button2.onclick = function(){
+		start();
+	}
+	button2.style.display = "inline";
+	button3.style.display = "inline";
+	button1.style.marginLeft = "0";
+	button1.style.marginTop = "450px";
+	description.style.display = "block";
+	description.innerHTML = "Waar wil je heen varen? Cannon Cove of Shipwreck Bay";
+	button1.onclick = function (){
+		cannoncove1();
+	}
+	button3.onclick = function (){
+		shipwrechbay();
+	}
+	item.style.display = "none";
+}
+
+function cannoncove1(){
+	body.style.backgroundImage = "url('https://i.redd.it/mggifxlvnor21.jpg')"
+	title.innerHTML = "Sea Adventure";
+	button1.innerHTML = "Vechten";
+	button3.innerHTML = "Terug keren";
+	button3.onclick = function(){
+		keuze1();
+	}
+	button1.onclick = function(){
+		dead();
+	}
+	button2.style.display = "none";
+	button3.style.display = "inline";
+	button1.style.marginLeft = "0";
+	button1.style.marginTop = "450px";
+	description.style.display = "block";
+	description.innerHTML = "Je komt een Kraken tegen wat doe je? Vechten of Terug Keren";
+	item.style.display = "none";
+}
+
+function dead(){
+	body.style.backgroundImage = "url('https://d1lss44hh2trtw.cloudfront.net/assets/article/2018/03/20/ghost-ship-sea-of-thieves_feature.jpg')"
+	title.innerHTML = "Sea Adventure";
+	description.style.display = "block";
+	description.innerHTML = "Je bent gezonken en overleden";
+	button2.style.display = "inline";
+	button2.onclick = function() {
+		start();
+	}
+	button1.style.display = "none";
+	button3.style.display = "none";
+	button2.innerHTML = "Opnieuw beginnen";
+	item.style.display = "none";
+}
+
+function shipwrechbay(){
+	body.style.backgroundImage = "url('https://gamepedia.cursecdn.com/seaofthieves_gamepedia/thumb/e/e8/ShipwreckBay_1.png/1200px-ShipwreckBay_1.png')"
+	title.innerHTML = "Sea Adventure";
+	description.style.display = "block";
+	description.innerHTML = "Je bent aangekomen bij Shipwreck Bay je ziet een Skeleton King wil je het Aanvallen of Door Varen?";
+	button1.style.display = "inline";
+	button3.style.display = "inline";
+	button2.style.display = "none";
+	button1.innerHTML = "Aanvallen";
+	button1.onclick = function(){
+		skaanvallen();
+	}
+	button3.innerHTML = "Dagger Tooth Outpost"
+	item.style.display = "none";
+	button3.onclick = function(){
+		daggertoothoutpost();
+	}
+}
+
+function daggertoothoutpost(){
+	body.style.backgroundImage = "url('https://vignette.wikia.nocookie.net/seaofthieves5135/images/d/dc/The-Hungering-Deep_Megalodon.jpg/revision/latest?cb=20180530181232')"
+	title.innerHTML = "Sea Adventure";
+	description.style.display = "block";
+	description.innerHTML = "Onderweg naar Dagger Tooth Outpost valt een Megalodon je aan wat doe je? Aanvallen of Terug keren";
+	button1.style.display = "inline";
+	button3.style.display = "inline";
+	button2.style.display = "none";
+	button1.innerHTML = "Aanvallen";
+	button1.onclick = function(){
+		if (itemclick == "1") {
+			einde();
+		}
+		else {
+			dead();
+		}
+	}
+	button3.innerHTML = "Terug Keren"
+	button3.onclick = function(){
+		shipwrechbay();
+	}
+	item.style.display = "none";
+}
+
+function skaanvallen(){
+	body.style.backgroundImage = "url('https://static.trueachievements.com/customimages/055257.jpg')"
+	title.innerHTML = "Sea Adventure";
+	description.style.display = "none";
+	button1.style.display = "none";
+	button2.style.display = "inline";
+	button2.innerHTML = "back";
+	button2.onclick = function(){
+		shipwrechbay();
+	}
+	item.style.display = "inline";
+	item.style.backgroundImage = "url('https://vignette.wikia.nocookie.net/seaofthieves/images/1/1d/Coffre_du_marin.png/revision/latest?cb=20180617113421&path-prefix=fr')";
+	item.onclick = function(){
+		itemclick = 1;
+		button3.style.display = "inline";
+		button3.innerHTML = "Cannon-Balls 1/1";
+	}
+	button3.style.display = "none";
+}
+
+function einde(){
+	body.style.backgroundImage = "url('https://d1lss44hh2trtw.cloudfront.net/assets/article/2018/03/26/how-to-customize-ship-sea-of-thieves-slim_1200x500.jpg')"
+	title.innerHTML = "Victory";
+	title.style.backgroundColor = "green";
+	button1.style.display = "none";
+	button2.style.display = "inline";
+	button2.innerHTML = "Restart Game";
+	button2.onclick = function(){
+		start();
+		location.reload();
+	}
+	button3.style.display = "none";
+	description.style.display = "none";
+	inventoryItem.style.display = "none";
+}
+
--- Week 1/labjs.js
+++ Week 1/labjs.js
@@ -1,9 +1,25 @@
-var opdrachten = prompt("Welke opdracht wil je? (1 | 2 | 3 | 4)");
+var opdrachten = prompt("Welke opdracht wil je? (1 | 2 | 3 | 4 | 5 | 6 )");
 if (opdrachten == "1") {
 	opdracht1();
 }
 else if (opdrachten == "2") {
 	opdracht2();
+}
+
+else if (opdrachten == "3") {
+	opdracht3();
+}
+
+else if (opdrachten == "4") {
+	opdracht4();
+}
+
+else if (opdrachten == "5") {
+	opdracht5();
+}
+
+else if (opdrachten == "6") {
+	opdracht6();
 }
 
 function opdracht1(){
@@ -45,10 +61,120 @@ function opdracht1(){
 }
 
 function opdracht2() {
-	var namenArray;
+	var namenArray = [];
 	var amountNames = prompt("Hoeveel namen wilt u in de array stoppen? (minimaal 3)");
-	for (i = 1; i <= amountNames; i++){
-		var newName = prompt("Welke naam wil je toevoegen?");
-		namenArray.push(newName);
+	if (amountNames <3) {
+		alert("Je moet minimaal 3");
+		opdracht2();
+	}
+	else {
+		for (i = 1; i <= amountNames; i++){
+			var newName = namenArray.push(prompt("Welke naam wil je toevoegen?"));
+			console.log(namenArray);
+		}
+	}
+	document.write("<h2>De ingevoerde namen in de array zijn:</h2>");
+	document.write(namenArray);
+	document.write("<br><br>");
+
+	namenArray.reverse();
+
+	document.write(namenArray);
+}
+
+function opdracht3() {
+	var arrayEen = [1,2,3,4,5,6,7,8,9,10];
+	var arrayTwee = [2,4,6,8,10,12,14,16,18,20];
+
+	document.write("<h2>Array operatoren<h2>");
+	document.write("<h2>Optellen van de twee arrays	zijn:<h2>");
+	
+	for (i = 0; i <= 9; i++){
+		document.write(arrayEen[i] + " + " + arrayTwee[i] + " = " + (arrayEen[i] + arrayTwee[i]));
+		document.write("<br>");
+	}
+
+	document.write("<br>");
+	document.write("<h2>Aftrekken van de twee arrays	zijn:<h2>");
+
+	for (i = 0; i <= 9; i++){
+		document.write(arrayTwee[i] + " - " + arrayEen[i] + " = " + (arrayTwee[i] - arrayEen[i]));
+		document.write("<br>");
+	}
+
+	document.write("<br>");
+	document.write("<h2>Vermenigvuldigen van de twee arrays	zijn:<h2>");
+
+	for (i = 0; i <= 9; i++){
+		document.write(arrayEen[i] + " * " + arrayTwee[i] + " = " + (arrayEen[i] * arrayTwee[i]));
+		document.write("<br>");
+	}
+
+	document.write("<br>");
+	document.write("<h2>Delen van de twee arrays zijn:<h2>");
+
+	for (i = 0; i <= 9; i++){
+		document.write(arrayTwee[i] + " / " + arrayEen[i] + " = " + (arrayTwee[i] / arrayEen[i]));
+		document.write("<br>");
+	}
+}
+
+function opdracht4() {
+	var getalen = [1,2,3,4,5,6,7,8,9,10];
+	var tafels = [2,4,6,8];
+
+	document.write("<h2>Tafel van 2:<h2>");
+
+	for (i = 0; i <= 9; i++){
+		document.write(getalen[i] + " * " + tafels[0] + " = " + (getalen[i] * tafels[0]));
+		document.write("<br>");
+	}
+
+	document.write("<br>");
+	document.write("<h2>Tafel van 4:<h2>");
+
+	for (i = 0; i <= 9; i++){
+		document.write(getalen[i] + " * " + tafels[1] + " = " + (getalen[i] * tafels[1]));
+		document.write("<br>");
+	}
+
+	document.write("<br>");
+	document.write("<h2>Tafel van 6:<h2>");
+
+	for (i = 0; i <= 9; i++){
+		document.write(getalen[i] + " * " + tafels[2] + " = " + (getalen[i] * tafels[2]));
+		document.write("<br>");
+	}
+
+	document.write("<br>");
+	document.write("<h2>Tafel van 8:<h2>");
+
+	for (i = 0; i <= 9; i++){
+		document.write(getalen[i] + " * " + tafels[3] + " = " + (getalen[i] * tafels[3]));
+		document.write("<br>");
+	}
+}
+
+function opdracht5() {
+	var getal = prompt("Voer getal in");
+	var array20 = [];
+
+	for (i = 0; i <= getal; i++){
+		array20.push(i);
+		document.write(array20.join(",") + "<br>");
+	}
+}
+
+function opdracht6() {
+	var getallen = [];
+
+	var test = parseInt(prompt("Vul een getal in"));
+	
+	for (var i = 0; i <= test; i++) {
+		getallen.push(i);
+	}
+	for (var i = 0; i <= test; i++) {
+		document.write(getallen.join("") + "<br>");
+		getallen.pop();
 	}
 }
